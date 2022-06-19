37bf1be136b8a27dd1e7800c98a3ca32e1c33e43
willPeng <panga0820@gmail.com>
2022-06-19 21:01:31 +0800

    Create Quack to implement quack behavior for duck

---
+++ HeadFirstDesignPatterns/HeadFirstDesignPatterns/Strategy/QuackBehavior/Quack.swift
@@ -0,0 +1,14 @@
+//
+//  Quack.swift
+//  HeadFirstDesignPatterns
+//
+//  Created by Will Peng on 2022/6/19.
+//
+
+import Foundation
+
+class Quack: QuackBehavior {
+    func quack() {
+        print("Implemented quack for duck")
+    }
+}
