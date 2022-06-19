02dd2f0792984f77b285113ec0f3162efba53f71
willPeng <panga0820@gmail.com>
2022-06-19 21:05:53 +0800

    Create MuteQuack to implement without quack behavior

---
+++ HeadFirstDesignPatterns/HeadFirstDesignPatterns/Strategy/QuackBehavior/MuteQuack.swift
@@ -0,0 +1,14 @@
+//
+//  MuteQuack.swift
+//  HeadFirstDesignPatterns
+//
+//  Created by Will Peng on 2022/6/19.
+//
+
+import Foundation
+
+class MuteQuack: QuackBehavior {
+    func quack() {
+        print("Don't do any thing")
+    }
+}
