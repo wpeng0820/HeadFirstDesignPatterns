d4eff116b17ab54df611eaf8fa43ff4e010b6769
willPeng <panga0820@gmail.com>
2022-06-19 21:04:18 +0800

    Create Squeak to implement quack behavior for Rubber

---
+++ HeadFirstDesignPatterns/HeadFirstDesignPatterns/Strategy/QuackBehavior/Squeak.swift
@@ -0,0 +1,14 @@
+//
+//  Squeak.swift
+//  HeadFirstDesignPatterns
+//
+//  Created by Will Peng on 2022/6/19.
+//
+
+import Foundation
+
+class Squeak: QuackBehavior {
+    func quack() {
+        print("Squeak")
+    }
+}
