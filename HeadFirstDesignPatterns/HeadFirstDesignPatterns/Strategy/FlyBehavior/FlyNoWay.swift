309070fd81c6575f1835711190b88c0c0499dcaf
willPeng <panga0820@gmail.com>
2022-06-19 20:59:54 +0800

    Create FlyNoWay to implement without fly behavior

---
+++ HeadFirstDesignPatterns/HeadFirstDesignPatterns/Strategy/FlyBehavior/FlyNoWay.swift
@@ -0,0 +1,14 @@
+//
+//  FlyNoWay.swift
+//  HeadFirstDesignPatterns
+//
+//  Created by Will Peng on 2022/6/19.
+//
+
+import Foundation
+
+class FlyNoWay: FlyBehavior {
+    func fly() {
+        print("Don't do any thing")
+    }
+}
