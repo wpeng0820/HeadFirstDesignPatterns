1dcc937c7bc6d97c555b80506b2434ad2974e4ae
willPeng <panga0820@gmail.com>
2022-06-19 20:58:13 +0800

    Create new FlyWithWings to implement fly befavior for duck

---
+++ HeadFirstDesignPatterns/HeadFirstDesignPatterns/Strategy/FlyBehavior/FlyWithWings.swift
@@ -0,0 +1,14 @@
+//
+//  FlyWithWings.swift
+//  HeadFirstDesignPatterns
+//
+//  Created by Will Peng on 2022/6/19.
+//
+
+import Foundation
+
+class FlyWithWings: FlyBehavior {
+    func fly() {
+        print("Implemented duck fly behavior")
+    }
+}
