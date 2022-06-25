0a4e6e8eeb4c08aac396687ced4a39347148ebf0
willPeng <panga0820@gmail.com>
2022-06-25 13:49:18 +0800

    First wrong implemented a way

--- HeadFirstDesignPatterns/HeadFirstDesignPatterns/Observer/WeatherData.swift
+++ HeadFirstDesignPatterns/HeadFirstDesignPatterns/Observer/WeatherData.swift
@@ -8,9 +8,25 @@
 import Foundation
 
 class WeatherData {
-    func getTemperature() { }
-    func getHumidiry() { }
-    func getPressure() { }
+    private let currentConditionsDisplay = CurrentConditionsDisplay()
+    private let statisticsDisplay = StatisticsDisplay()
+    private let forecasDisplay = ForecasDisplay()
+    
+    
+    func getTemperature() -> String { "Temperature" }
+    
+    func getHumidiry() -> String { "Humidiry" }
+    
+    func getPressure() -> String { "Pressure" }
+    
     // This method will invoked after the object get new data
-    func measurementsChanged() { }
+    func measurementsChanged() {
+        let temp = getTemperature()
+        let humidiry = getHumidiry()
+        let pressure = getPressure()
+        
+        currentConditionsDisplay.update(temp: temp, humidity: humidiry, pressure: pressure)
+        statisticsDisplay.update(temp: temp, humidity: humidiry, pressure: pressure)
+        forecasDisplay.update(temp: temp, humidity: humidiry, pressure: pressure)
+    }
 }
