8b24915a0e9734d42a46fb5fefccf4c2797c6b8e
willPeng <panga0820@gmail.com>
2022-06-25 13:44:40 +0800

    Add a new ForecasDisplay class

---
+++ HeadFirstDesignPatterns/HeadFirstDesignPatterns/Observer/ForecasDisplay.swift
@@ -0,0 +1,12 @@
+//
+//  ForecasDisplay.swift
+//  HeadFirstDesignPatterns
+//
+//  Created by Will Peng on 2022/6/25.
+//
+
+import Foundation
+
+class ForecasDisplay {
+    func update(temp: String, humidity: String, pressure: String) { }
+}
