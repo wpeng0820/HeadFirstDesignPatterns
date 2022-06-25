880590a706f8264be0a3a9d50664d0534e2e05e1
willPeng <panga0820@gmail.com>
2022-06-25 13:43:33 +0800

    Add a new StatisticsDisplay class

---
+++ HeadFirstDesignPatterns/HeadFirstDesignPatterns/Observer/StatisticsDisplay.swift
@@ -0,0 +1,12 @@
+//
+//  StatisticsDisplay.swift
+//  HeadFirstDesignPatterns
+//
+//  Created by Will Peng on 2022/6/25.
+//
+
+import Foundation
+
+class StatisticsDisplay {
+    func update(temp: String, humidity: String, pressure: String) { }
+}
