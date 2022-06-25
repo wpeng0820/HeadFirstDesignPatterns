fa2e5d038f225d4f93873cd79b5e912b0630100a
willPeng <panga0820@gmail.com>
2022-06-25 13:42:42 +0800

    Create a new CurrentConditionsDisplay

---
+++ HeadFirstDesignPatterns/HeadFirstDesignPatterns/Observer/CurrentConditionsDisplay.swift
@@ -0,0 +1,12 @@
+//
+//  CurrentConditionsDisplay.swift
+//  HeadFirstDesignPatterns
+//
+//  Created by Will Peng on 2022/6/25.
+//
+
+import Foundation
+
+class CurrentConditionsDisplay {
+    func update(temp: String, humidity: String, pressure: String) { }
+}
