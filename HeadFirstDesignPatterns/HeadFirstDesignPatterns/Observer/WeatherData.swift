ffb45f45143c3d8541c5d91146300702e63ddf9d
willPeng <panga0820@gmail.com>
2022-06-25 13:38:23 +0800

    Create a weatherData class

---
+++ HeadFirstDesignPatterns/HeadFirstDesignPatterns/Observer/WeatherData.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherData.swift
+//  HeadFirstDesignPatterns
+//
+//  Created by Will Peng on 2022/6/25.
+//
+
+import Foundation
+
+class WeatherData {
+    func getTemperature() { }
+    func getHumidiry() { }
+    func getPressure() { }
+    // This method will invoked after the object get new data
+    func measurementsChanged() { }
+}
